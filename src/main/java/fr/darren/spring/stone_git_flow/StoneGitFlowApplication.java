1e71594175816ab799bb8a28ce9f657089f945b3
Darren BOUTROS <darren.boutros@gmail.com>
2023-05-29 21:34:08 +0200

    init project

---
+++ src/main/java/fr/darren/spring/stone_git_flow/StoneGitFlowApplication.java
@@ -0,0 +1,13 @@
+package fr.darren.spring.stone_git_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoneGitFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoneGitFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/darren/spring/stone_git_flow/StoneGitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.darren.spring.stone_git_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoneGitFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
